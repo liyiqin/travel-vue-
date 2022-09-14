55494a0674cd0c6738f365aecfb03a7ce0d87559
liyiqing <1421251960@qq.com>
2022-09-14 12:15:39 +0800

    prefomance

--- src/pages/city/componets/List.vue
+++ src/pages/city/componets/List.vue
@@ -17,7 +17,7 @@
             </div>
         </div>
     </div>
-    <div class="area" v-for="(item,key) of cities" :key="key">
+    <div class="area" v-for="(item,key) of cities" :key="key" :ref="key">
         <div class="title border-topbottom">{{key}}</div>
         <div class="item-list">
             <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">{{innerItem.name}}</div>
@@ -33,13 +33,26 @@ export default {
   name: 'CityList',
   props: {
     hot: Array,
-    cities: Object
+    cities: Object,
+    letter: String
   },
   mounted () {
-    this.scroll = new Bscroll(this.$refs.wrapper)
+    this.scroll = new Bscroll(this.$refs.wrapper, {
+      click: true,
+      disableMouse: false,
+      disableTouch: false
+    })
   },
   updated () {
     this.scroll && this.scroll.refresh()
+  },
+  watch: {
+    letter () {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        this.scroll.scrollToElement(element)
+      }
+    }
   }
 }
 </script>
